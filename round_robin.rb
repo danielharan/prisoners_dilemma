e1ce596ad36dd781c14705f2156c9608f2ea7045
Daniel Haran <chebuctonian@mgmail.com>
2011-09-15 18:23:08 +0900

    tease out the round robin logic

--- round_robin.rb
+++ round_robin.rb
@@ -7,24 +7,29 @@ class RoundRobin
   # this stands for now because it's ~20-30X faster for a round of 100 instances
   def initialize(strategies)
     cached_results = {}
-    strategies.each do |strat1|
-      (strategies - [strat1]).each do |strat2|
-        if (cached_result = cached_results[[strat1.class,strat2.class]])
-          strat1.score += cached_result[0]
-          strat2.score += cached_result[1]
-        else
-          strat1_score_cache, strat2_score_cache = strat1.score, strat2.score
-          m = Match.new(strat1, strat2)
-          m.run
-          
-          cached_results[[strat1.class,strat2.class]] = [strat1.score, strat2.score]
-          
-          strat1.score += strat1_score_cache
-          strat2.score += strat2_score_cache
-        end
+    self.class.round_robin(strategies) do |strat1, strat2|
+      if (cached_result = cached_results[[strat1.class,strat2.class]])
+        strat1.score += cached_result[0]
+        strat2.score += cached_result[1]
+      else
+        strat1_score_cache, strat2_score_cache = strat1.score, strat2.score
+        m = Match.new(strat1, strat2)
+        m.run
+        
+        cached_results[[strat1.class,strat2.class]] = [strat1.score, strat2.score]
+        
+        strat1.score += strat1_score_cache
+        strat2.score += strat2_score_cache
       end
-      strategies -= [strat1] # true round-robin; otherwise it gets to play twice
     end
   end
-  
+
+  def self.round_robin(elements)
+    elements.each do |el1|
+      (elements - [el1]).each do |el2|
+        yield el1, el2
+      end
+      elements -= [el1]
+    end
+  end
 end
